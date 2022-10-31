6e9fddb08aed09b2ae0368cce1850598d7ddf21e
Egor Yakovin <yakovin.e@yandex.ru>
2022-10-31 21:00:08 +0300

    Setup circles

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -15,7 +15,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 10
+        redLight.layer.cornerRadius = 75
+        redLight.backgroundColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.3)
+        yellowLight.layer.cornerRadius = 75
+        yellowLight.backgroundColor = UIColor(red: 255, green: 237, blue: 0, alpha: 0.3)
+        greenLight.layer.cornerRadius = 75
+        greenLight.backgroundColor = UIColor(red: 0, green: 255, blue: 0, alpha: 0.3)
+        
     }
 
 
